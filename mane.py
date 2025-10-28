9974532e2147f06e9e6189c526afd341ef713d69
789mmm <bindurajpurohit91@gmail.com>
2025-10-27 18:37:34 -0700

    Add files via upload

---
+++ mane.py
@@ -0,0 +1,13 @@
+age = 15
+weight = 38.5
+print("Age :", age)
+print("Data type of age is", type(age))
+print("weight :", weight)
+print("Data type of weight is", type(weight))
+print("/n After type casting...")
+age = str(age)
+print(age)
+print("data type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("data type of weight is", type(weight))

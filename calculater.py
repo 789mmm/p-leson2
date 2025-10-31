54836d36069c5b9d44653180237ba045945d87f0
789mmm <bindurajpurohit91@gmail.com>
2025-10-30 18:43:24 -0700

    Add files via upload

---
+++ calculater.py
@@ -0,0 +1,10 @@
+print("Enter Mark obtaind in 4 subject: " )
+math = int(input("math :"))
+english = int(input("english :"))
+hindi = int(input("hindi :"))
+scince = int(input("scince :")) 
+sum = math+english+scince+hindi
+print("sum of math,scince,hindi,english")
+perc = (sum/400)*100
+print(end="percentage mark = ")
+print(perc)          
